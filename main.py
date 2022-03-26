ab26585dda0745ec208675a60aa7e9b2d0522445
674xu8 <enginseer@enginseer.ru>
2022-03-26 22:26:39 +0700

    remove empty lines

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ client_id = f'python-mqtt-{random.randint(0, 1000)}'
 # username = 'emqx'
 # password = 'public'
 
+
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
-
     client = mqtt_client.Client(client_id)
 #    client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
+
 def publish(client):
     msg = "test"+str(random.randint(0, 1000))
-    
     result = client.publish(topic, msg)
     # result: [0, 1]
     status = result[0]
@@ -36,14 +36,14 @@ def publish(client):
         print(f"Send `{msg}` to topic `{topic}`")
     else:
         print(f"Failed to send message to topic {topic}")
-    
+
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
     client.subscribe(topic+"2")
     client.on_message = on_message
+
 
 def run():
     client = connect_mqtt()
@@ -54,6 +54,7 @@ def run():
         while keyboard.is_pressed('ctrl'):
             time.sleep(0.5)
         publish(client)
+
 
 if __name__ == '__main__':
     run()
